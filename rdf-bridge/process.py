67a3485922423f70a815afa4bb04849610256a09
waqar-alamgir <waqarcs@yahoo.com>
2018-07-01 12:43:09 +0200

    Added a new param to print in process.py

--- rdf-bridge/process.py
+++ rdf-bridge/process.py
@@ -11,12 +11,14 @@ if sys.version[0] == '2':
 pid = psutil.Process().pid
 logger_obj = logger.Logger(filename='rdf-bridge-process.log', instance_id=pid)
 executer_obj = executer.Executer()
-logger_obj.write_log('Program is started with process id: '+str(pid), 1, False)
-
 file_path_source = sys.argv[1]
 start_line = sys.argv[2]
 end_line = sys.argv[3]
 job_id = sys.argv[4]
+to_print = False
+if len(sys.argv) == 6:
+    to_print = True
+logger_obj.write_log('Program is started with process id: '+str(pid), 1, to_print)
 
 file_path_destination = '../bitmat-data-store/out_'+start_line+'_'+end_line+'_'+job_id
 file_obj = open(file_path_destination, 'w')
@@ -29,29 +31,29 @@ for n,line in enumerate(fp):
         data_ids = []
         
         cmd = 'grep -n \''+data[0]+'\' '+file_path_source+'-sub-all'
-        output = executer_obj.run(cmd, [], False)
+        output = executer_obj.run(cmd, [], to_print)
 
         if output[0] == 0:
             result = output[1]
-            logger_obj.write_log('Result fo sub on line '+str(n+1)+' with res: '+result, 1, False)
+            logger_obj.write_log('Result fo sub on line '+str(n+1)+' with res: '+result, 1, to_print)
             result = result.split(':', 1)
             if len(result) == 2:
                 data_ids.append(result[0])
 
         cmd = 'grep -n \''+data[1]+'\' '+file_path_source+'-pre-all'
-        output = executer_obj.run(cmd, [], False)
+        output = executer_obj.run(cmd, [], to_print)
         if output[0] == 0:
             result = output[1]
-            logger_obj.write_log('Result fo pre on line '+str(n+1)+' with res: '+result, 1, False)
+            logger_obj.write_log('Result fo pre on line '+str(n+1)+' with res: '+result, 1, to_print)
             result = result.split(':', 1)
             if len(result) == 2:
                 data_ids.append(result[0])
 
         cmd = 'grep -n \''+data[2]+'\' '+file_path_source+'-obj-all'
-        output = executer_obj.run(cmd, [], False)
+        output = executer_obj.run(cmd, [], to_print)
         if output[0] == 0:
             result = output[1]
-            logger_obj.write_log('Result fo obj on line '+str(n+1)+' with res: '+result, 1, False)
+            logger_obj.write_log('Result fo obj on line '+str(n+1)+' with res: '+result, 1, to_print)
             result = result.split(':', 1)
             if len(result) == 2:
                 data_ids.append(result[0])
@@ -59,7 +61,7 @@ for n,line in enumerate(fp):
         if len(data_ids) == 3:
             file_obj.write(data_ids[0]+':'+data_ids[1]+':'+data_ids[2]+"\n")
         else:
-            logger_obj.write_log('Data length on line '+str(n+1)+' is not 3: '+str(len(data_ids)), 3, False)
+            logger_obj.write_log('Data length on line '+str(n+1)+' is not 3: '+str(len(data_ids)), 3, to_print)
 
 fp.close()
 file_obj.close()
