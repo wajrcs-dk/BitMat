c08f75bc3b729a9b91398486882bad6b078f4e23
waqar-alamgir <waqarcs@yahoo.com>
2018-10-15 00:36:27 +0200

    Changes in cmd for time

---
+++ rdf-query-interface/test.py
@@ -0,0 +1,17 @@
+
+import executer
+
+executer_obj = executer.Executer()
+
+for i in range(1, 14):
+    cmd = 'python rdf-query-interface/cmd.py '+str(i)+' "" config/lubm15gb.conf 1'
+    outputList = '=sum('
+
+    for j in range(1, 10):
+        outputList = outputList + executer_obj.run(cmd, print_result=False)[2]
+        if j <=9:
+            outputList = outputList + ', '
+
+    outputList = outputList + ')/10'
+
+    print outputList
